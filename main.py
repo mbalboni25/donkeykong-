fce89dd4323d2e6ece39251128a367ac827be940
Can <115503146+canTuncPekkan@users.noreply.github.com>
2023-07-14 19:05:13 +0000

    made class names

---
+++ main.py
@@ -0,0 +1,15 @@
+class Ground:
+    def __init__(self) -> None:
+            pass
+    
+class JumpMan:
+      def __init__(self) -> None:
+            pass
+
+class Barrel:
+      def __init__(self) -> None:
+            pass
+
+class Monkey:
+      def __init__(self) -> None:
+            pass
