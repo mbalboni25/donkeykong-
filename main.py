bef85e55ffb94e5df5cfd28bbdb00cdcb8a9e356
Can <115503146+canTuncPekkan@users.noreply.github.com>
2023-07-14 19:49:43 +0000

    added basic pygame loop along with window opener

--- main.py
+++ main.py
@@ -19,3 +19,32 @@ class Barrel:
 class Monkey:
     def __init__(self) -> None:
         pass
+
+
+# a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((504, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window or CMD+Q on mac
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    # RENDER GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
